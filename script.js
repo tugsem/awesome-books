fcf667cf3a724492e979fe80c209e4b9d93ad20c
Tugsem <saranlitugsem@gmail.com>
2022-04-18 16:29:34 +0300

    Add homepage

--- script.js
+++ script.js
@@ -3,28 +3,27 @@ const authorInput = document.querySelector('#author');
 const addBtn = document.querySelector('#add');
 const bookContainer = document.querySelector('.container');
 const bookDiv = document.createElement('div');
-bookDiv.classList.add('added-book');
-const removeBtn = document.createElement('button');
+bookDiv.classList.add('added-book remove');
+
 
 const booksArray = [];
-class newBook {
-    constructor(title, author) {
-        this.title = title;
-        this.author = author;
-    }
-}
+
 addBtn.addEventListener('click', () => {
     let title = titleInput.value;
     let author = authorInput.value;
-    let addObj = new newBook(title, author);
-    booksArray.push(addObj);
+    const obj = {};
+    obj.title = title;
+    obj.author = author;
+    booksArray.push(obj);
     let createNewTitle = document.createElement('h2');
     createNewTitle.textContent = title;
     let createNewAuthor = document.createElement('h3');
     createNewAuthor.innerText = author;
+    const removeBtn = document.createElement('button');
     removeBtn.innerText ="Remove";
     bookDiv.append(createNewTitle, createNewAuthor, removeBtn);
     bookContainer.append(bookDiv);
-    console.log(booksArray);
+    console.log(bookDiv.button);
 })
+
 
