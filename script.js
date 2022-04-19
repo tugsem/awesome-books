089109d5ad23feddec69b267a837d236286f202d
Tugsem <saranlitugsem@gmail.com>
2022-04-19 17:59:48 +0300

    Solve remove button issue

--- script.js
+++ script.js
@@ -1,70 +1,42 @@
+const form = document.querySelector('#form');
 const container = document.querySelector('.container');
-let books = [];
-container.innerHTML = `
-<div class="books"></div>
-<form>
-    <input type="text" class="title" placeholder="Title" required><br><br>
-    <input type="text" class="author" placeholder="Author" required class><br><br>
-    <button class="add">Add</button>
-    </form>
-`;
+const storage = window.localStorage;
 
-function NewBooks(title, author) {
-  this.title = title;
-  this.author = author;
-}
+let bookList = [];
 
-const bookContainer = container.querySelector('.books');
-const add = container.querySelector('.add');
-const title = container.querySelector('.title');
-const author = container.querySelector('.author');
-
-function addBooks(title, author) {
-  const book = new NewBooks(title, author);
-  books.push(book);
-}
-
-function editLocalStorage(index) {
-  books = JSON.parse(localStorage.books);
-  const data = books.filter((book) => book !== books[index]);
-  localStorage.setItem('books', JSON.stringify(data));
+function displayBooks() {
+  const books = JSON.parse(storage.getItem('books'));
+  if (books != null) {
+    bookList = books;
+  }
+  let bookContainer = '';
+  for (let i = 0; i < bookList.length; i += 1) {
+    bookContainer += `<div>
+                    <p>${bookList[i].title}</p>
+                    <p>${bookList[i].author}</p>
+                    <button type="submit" class="rmvBtn" onclick="removeBook(${i})">Remove</button>
+                    <hr />
+                  </div>`;
+  }
+  container.innerHTML = bookContainer;
 }
 
 function removeBook(index) {
-  books = books.filter((item) => item !== books[index]);
+  const newList = bookList.filter((_, i) => i !== index);
+  storage.setItem('books', JSON.stringify(newList));
+  window.onload = displayBooks();
 }
 
-function displayBooks(arr) {
-  bookContainer.innerHTML = '';
-  for (let i = 0; i < arr.length; i += 1) {
-    const x = ` 
-    <p>${arr[i].title}</p>
-    <p>${arr[i].author}</p>
-    <button class="remove">Remove</button>
-    <hr/>
-    `;
-    bookContainer.innerHTML += x;
-  }
-  const remove = container.querySelectorAll('.remove');
-  remove.forEach((btn, index) => btn.addEventListener('click', () => {
-    removeBook(index);
-    displayBooks(books);
-    editLocalStorage(index);
-  }));
+function addBook() {
+  const title = document.querySelector('#title');
+  const author = document.querySelector('#author');
+  bookList.push({ title: title.value, author: author.value });
+  storage.setItem('books', JSON.stringify(bookList));
+  displayBooks();
+  title.value = '';
+  author.value = '';
 }
 
-add.addEventListener('click', (e) => {
-  if (title.value !== '' && author.value !== '') {
-    e.preventDefault();
-    addBooks(title.value, author.value);
-    localStorage.setItem('books', JSON.stringify(books));
-    displayBooks(books);
-    title.value = '';
-    author.value = '';
-  }
-});
+form.addEventListener('submit', addBook);
 
-window.addEventListener('DOMContentLoaded', () => {
-  books = JSON.parse(localStorage.books);
-  displayBooks(books);
-});
+window.onload = displayBooks();
