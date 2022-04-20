ac037d8319866a5a23b96a4f0014420fe4b427a0
Tugsem <saranlitugsem@gmail.com>
2022-04-20 14:51:07 +0300

    Add javacript class and methods

--- script.js
+++ script.js
@@ -1,42 +1,54 @@
 const form = document.querySelector('#form');
 const container = document.querySelector('.container');
 const storage = window.localStorage;
+const titleInp = document.querySelector('#title');
+const authorInp = document.querySelector('#author');
 
-let bookList = [];
-
-function displayBooks() {
-  const books = JSON.parse(storage.getItem('books'));
-  if (books != null) {
-    bookList = books;
+class Book {
+  constructor() {
+    this.bookList = JSON.parse(storage.getItem('books')) || [];
   }
-  let bookContainer = '';
-  for (let i = 0; i < bookList.length; i += 1) {
-    bookContainer += `<div>
-                    <p>${bookList[i].title}</p>
-                    <p>${bookList[i].author}</p>
-                    <button type="submit" class="rmvBtn" onclick="removeBook(${i})">Remove</button>
-                    <hr />
-                  </div>`;
+
+  addBook(title, author) {
+    const book = { title, author };
+    this.bookList.push(book);
+    storage.setItem('books', JSON.stringify(this.bookList));
+    this.displayBooks();
+    titleInp.value = '';
+    authorInp.value = '';
   }
-  container.innerHTML = bookContainer;
+
+  displayBooks() {
+    this.bookList = JSON.parse(storage.getItem('books'));
+    let bookContainer = '';
+    for (let i = 0; i < this.bookList.length; i += 1) {
+      bookContainer += `<div>
+                          <p>${this.bookList[i].title}</p>
+                          <p>${this.bookList[i].author}</p>
+                          <button type="submit" class="rmvBtn" onclick="remove(${i})">Remove</button>
+                          <hr />
+                        </div>`;
+    }
+    container.innerHTML = bookContainer;
+  }
+
+  removeBook(index) {
+    const newList = this.bookList.filter((_, i) => i !== index);
+    storage.setItem('books', JSON.stringify(newList));
+    this.displayBooks();
+  }
 }
-/* eslint-disable no-unused-vars */
-function removeBook(index) {
-  const newList = bookList.filter((_, i) => i !== index);
-  storage.setItem('books', JSON.stringify(newList));
-  window.onload = displayBooks();
-}
-/* eslint-enable no-unused-vars */
-function addBook() {
-  const title = document.querySelector('#title');
-  const author = document.querySelector('#author');
-  bookList.push({ title: title.value, author: author.value });
-  storage.setItem('books', JSON.stringify(bookList));
-  displayBooks();
-  title.value = '';
-  author.value = '';
+const book = new Book();
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  if (titleInp.value && authorInp.value) {
+    book.addBook(titleInp.value, authorInp.value);
+  }
+});
+
+function remove(index) { //eslint-disable-line
+  book.removeBook(index);
 }
 
-form.addEventListener('submit', addBook);
-
-window.onload = displayBooks();
+book.displayBooks();
