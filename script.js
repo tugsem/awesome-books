1bb15d5d9e3cdca3c76d71f8590c9a9b5757bc4e
Tugsem <saranlitugsem@gmail.com>
2022-04-20 18:50:48 +0300

    Make changes

--- script.js
+++ script.js
@@ -3,56 +3,72 @@ const container = document.querySelector('.container');
 const storage = window.localStorage;
 const titleInp = document.querySelector('#title');
 const authorInp = document.querySelector('#author');
+let bookCollection = JSON.parse(storage.getItem('books') || []);
 
 class Book {
-  constructor() {
-    this.bookList = JSON.parse(storage.getItem('books')) || [];
-  }
-
-  addBook(title, author) {
-    const book = { title, author };
-    this.bookList.push(book);
-    storage.setItem('books', JSON.stringify(this.bookList));
-    this.displayBooks();
-    titleInp.value = '';
-    authorInp.value = '';
-  }
-
-  displayBooks() {
-    this.bookList = JSON.parse(storage.getItem('books'));
-    let bookContainer = '';
-    for (let i = 0; i < this.bookList.length; i += 1) {
-      bookContainer += `
-                        <div class="single-book">
-                          <div class="bio">
-                            <h3>${this.bookList[i].title}</h3>
-                            <span>by</span>
-                            <h3>${this.bookList[i].author}</h3>
-                          </div>
-                          <button type="submit" class="rmvBtn" onclick="remove(${i})">Remove</button>
-                        </div>`;
-    }
-    container.innerHTML = bookContainer;
-    container.className = 'List';
-  }
-
-  removeBook(index) {
-    const newList = this.bookList.filter((_, i) => i !== index);
-    storage.setItem('books', JSON.stringify(newList));
-    this.displayBooks();
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+    this.id = Date.now();
   }
 }
-const book = new Book();
+
+const methods = {
+  
+  add() {
+    const book = new Book(titleInp.value, authorInp.value);
+
+    bookCollection.push(book);
+    storage.setItem('books', JSON.stringify(bookCollection));
+    container.innerHTML += `<div id="${book.id}">
+                          <p>${book.title}</p>
+                          <p>${book.author}</p>
+                          <button type="submit" class="rmvBtn">Remove</button>
+                          <hr />
+                        </div>`;
+
+    titleInp.value = '';
+    authorInp.value = '';
+  },
+
+  display() {
+    bookCollection = JSON.parse(storage.getItem('books')) || [];
+    let bookContainer = '';
+    bookCollection.forEach((book) => {
+      bookContainer += `<div id="${book.id}">
+                          <p>${book.title}</p>
+                          <p>${book.author}</p>
+                          <button type="submit" class="rmvBtn">Remove</button>
+                          <hr />
+                        </div>`;
+    });
+    container.innerHTML = bookContainer;
+  },
+
+  remove(id) {
+    bookCollection = JSON.parse(storage.getItem('books'));
+    bookCollection.forEach((book) => {
+      if(book.id == id ) { //eslint-disable-line
+        const index = bookCollection.indexOf(book);
+        bookCollection.splice(index, 1);
+      }
+    });
+    storage.setItem('books', JSON.stringify(bookCollection));
+    this.display();
+  },
+};
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  if (titleInp.value && authorInp.value) {
-    book.addBook(titleInp.value, authorInp.value);
+  methods.add();
+});
+
+container.addEventListener('click', (e) => {
+  if(e.target.className == "rmvBtn") {  //eslint-disable-line
+    const element = e.target.parentNode;
+    element.remove();
+    methods.remove(element.id);
   }
 });
 
-function remove(index) { //eslint-disable-line
-  book.removeBook(index);
-}
-
-book.displayBooks();
+window.onload = methods.display();
